6efda2a4cdf9f7c66edc4dde81bede30fc4abbf9
scardenol <61295467+scardenol@users.noreply.github.com>
2024-01-02 17:49:37 -0500

    Update 6_algorithm_design_shortest_path.py
    Added comments for clarity

--- Scientific Computing with Python/6_algorithm_design_shortest_path.py
+++ Scientific Computing with Python/6_algorithm_design_shortest_path.py
@@ -4,6 +4,7 @@
 # In this project, you'll learn how to use functions, loops, conditional statements, and
 # dictionary comprehensions to implement a Shortest Path algorithm.
 
+# Define a graph
 my_graph = {
     "A": [("B", 5), ("C", 3), ("E", 11)],
     "B": [("A", 5), ("C", 1), ("F", 2)],
@@ -14,32 +15,47 @@ my_graph = {
 }
 
 
+# Define a function to find the shortest path
 def shortest_path(graph, start, target=""):
-    unvisited = list(graph)
-    distances = {node: 0 if node == start else float("inf") for node in graph}
-    paths = {node: [] for node in graph}
-    paths[start].append(start)
+    # Initialize the variables
+    unvisited = list(graph)  # unvisited nodes
+    distances = {node: 0 if node == start else float("inf") for node in graph}  # distances from start
+    paths = {node: [] for node in graph}  # paths from start
+    paths[start].append(start)  # append start to paths as we start from here
 
+    # Find the shortest path
     while unvisited:
+        # get the node with the smallest distance
         current = min(unvisited, key=distances.get)
+        # for each neighbor of current node
         for node, distance in graph[current]:
+            # if the total distance is smaller than the neighbor's distance
             if distance + distances[current] < distances[node]:
+                # update the neighbor's distance
                 distances[node] = distance + distances[current]
+                # update the neighbor's path with the current node
                 if paths[node] and paths[node][-1] == node:
                     paths[node] = paths[current][:]
+                # if the neighbor is not in the path, extend the neighbor's path with the current node
                 else:
                     paths[node].extend(paths[current])
+                # append the neighbor to the path
                 paths[node].append(node)
+        # remove the current node from the unvisited nodes
         unvisited.remove(current)
 
+    # Print the shortest path
     targets_to_print = [target] if target else graph
     for node in targets_to_print:
+        # Ignore the start node
         if node == start:
             continue
+        # Print the shortest path from the start node to the target node and the distance
         print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
 
     return distances, paths
 
 
+# Call the function to find the shortest path from A to F. Run only if this file is executed.
 if __name__ == "__main__":
     _, _ = shortest_path(my_graph, "A", "F")
